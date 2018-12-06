c23ee97b7ec0d3a69678d04c1e8903dcbe83f848
Boris Zekic <bzekic@spartaglobal.com>
2018-12-06 01:13:48 +0000

    bbc sign in page pom

--- src/test/java/com/sparta/bz/BbcSite/BbcPages/BbcSignInPage.java
+++ src/test/java/com/sparta/bz/BbcSite/BbcPages/BbcSignInPage.java
@@ -14,30 +14,28 @@ public class BbcSignInPage {
     private By passwordFieldId = By.id("password-input");
     private By submitButtonId = By.id("submit-button");
     private By incorrectPasswordErrorId = By.id("form-message-password");
+    private String errorText = "Sorry, that password isn't valid. Please include a letter.";
 
     public BbcSignInPage(WebDriver driver) {
         this.driver = driver;
     }
-
     public void goToSignInPage(){
         driver.navigate().to(signInPageURL);
     }
-
     public void inputUserName(String name){
         driver.findElement(usernameFieldId).sendKeys(name);
     }
-
     public void inputPassword(String password){
         driver.findElement(passwordFieldId).sendKeys(password);
     }
-
     public void clickSubmitButton(){
         driver.findElement(submitButtonId).click();
     }
-
     public String getPasswordErrorText(){
         return driver.findElement(incorrectPasswordErrorId).getText();
     }
+    public String getSignInPageURL() { return signInPageURL; }
+    public String getErrorText() { return errorText; }
 
     //page assertion
 
